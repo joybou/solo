92bd8291f60ae84dc304166297ba1fdf03743fb0
joybou <846015234@qq.com>
2019-08-26 23:45:03 +0800

    Update docker-restart.sh

--- scripts/docker-restart.sh
+++ scripts/docker-restart.sh
@@ -16,4 +16,4 @@ docker run --detach --name solo --network=host \
     --env JDBC_PASSWORD="123456" \
     --env JDBC_DRIVER="com.mysql.cj.jdbc.Driver" \
     --env JDBC_URL="jdbc:mysql://127.0.0.1:3306/solo?useUnicode=yes&characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC" \
-    b3log/solo --listen_port=8080 --server_scheme=http --server_host=localhost
+    b3log/solo --listen_port=8080 --server_scheme=http --server_host=www.kidcaoblog.com --server_port=
